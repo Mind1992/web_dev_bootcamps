217c35bea6aa3ec1ba96bb7708b30c1027552a86
Sergii Mertsalov <sergmerts@gmail.com>
2014-08-11 20:52:18 -0700

    read bootcamps' attributes

--- spec/bootcamp_spec.rb
+++ spec/bootcamp_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe 'Bootcamp' do 
-	it 'initializes bootcamp with name and length_id' do 
+	it 'initializes bootcamp with id, name and length_id' do 
 		new_bootcamp = Bootcamp.new({name: 'Epicodus', length_id: nil})
+	end
+
+	it "lets you read bootcamps' name" do 
+		new_bootcamp = Bootcamp.new({name: 'Epicodus', length_id: nil})
+		expect(new_bootcamp.name).to eq "Epicodus"
 	end
 end
